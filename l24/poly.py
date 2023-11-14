42a52325f1716a3910db06862151b924152681aa
Himanshu <himanshu@MacBook-Pro-14-inch-2973.local>
2023-11-14 17:28:53 +0530

    after class 24

--- l24/poly.py
+++ l24/poly.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 xn = np.linspace(-2,2,100)
-for i in range(2,5):
-  yn = xn**i
+for i in range(2,100):
+  yn = (xn)**i+xn
   print (i) 
   plt.plot(xn,yn)
 plt.grid()
-#plt.xlim(-0.01, 0.01)
-plt.ylim(-0.01, 0.01)
+plt.xlim(-1,1)
+plt.ylim(-0.25, 0.25)
 plt.savefig('polynominal.png')
--- l24/spline_test.py
+++ l24/spline_test.py
@@ -1,14 +1,16 @@
 from scipy.interpolate import CubicSpline
 import numpy as np
 import matplotlib.pyplot as plt
-mat=np.loadtxt("test.dat")
+mat=np.loadtxt("xy.dat")
 x = mat[:,0]
 y = mat[:,1]
-xs = np.arange(-2.4, 1.2, 0.1)
+xs = np.arange(2,6,0.01)
+#xs = np.arange(-2.4, 1.2, 0.1)
 
 cs = CubicSpline(x, y)
+print (cs)
 fig, ax = plt.subplots(figsize=(6.5, 4))
 ax.plot(x, y, 'o', label='data')
 ax.plot(xs, cs(xs), label="S")
-plt.show()
-plt.savefig("spline.png")
+plt.legend()
+plt.savefig('spline.png')
